8c8f30697385ba609b401c45a24c4682ee371a9d
syjintw <yuanjiun911@outlook.com>
2026-04-22 01:07:05 +0800

    Update latest version

--- diff_gaussian_rasterization_lapisgs/__init__.py
+++ diff_gaussian_rasterization_lapisgs/__init__.py
@@ -30,7 +30,7 @@ def rasterize_gaussians(
     cov3Ds_precomp,
     raster_settings,
 ):  
-    print("rasterize_gaussians")
+    # print("rasterize_gaussians")
     return _RasterizeGaussians.apply(
         means3D,
         means2D,
@@ -59,7 +59,7 @@ class _RasterizeGaussians(torch.autograd.Function):
         cov3Ds_precomp,
         raster_settings,
     ):
-        print("_RasterizeGaussians --> forward") # [YC] debug
+        # print("_RasterizeGaussians --> forward") # [YC] debug
         
         # Flatten from CxHxW
         bg = raster_settings.bg if len(raster_settings.bg.shape) == 1 else raster_settings.bg.permute(1,2,0).flatten()
@@ -103,17 +103,17 @@ class _RasterizeGaussians(torch.autograd.Function):
                 print("\nAn error occured in forward. Please forward snapshot_fw.dump for debugging.")
                 raise ex
         else:
-            print("rasterize_gaussians") # [YC] debug
+            # print("rasterize_gaussians") # [YC] debug
             # num_rendered, color, radii, geomBuffer, binningBuffer, imgBuffer = _C.rasterize_gaussians(*args)
             num_rendered, color, radii, geomBuffer, binningBuffer, imgBuffer, final_opacity = _C.rasterize_gaussians(*args)
 
         # Keep relevant tensors for backward
         ctx.raster_settings = raster_settings
-        print("raster_settings") # [YC] debug
+        # print("raster_settings") # [YC] debug
         ctx.num_rendered = num_rendered
-        print("num_rendered") # [YC] debug
+        # print("num_rendered") # [YC] debug
         ctx.save_for_backward(colors_precomp, means3D, scales, rotations, cov3Ds_precomp, radii, sh, geomBuffer, binningBuffer, imgBuffer)
-        print("save_for_backward") # [YC] debug
+        # print("save_for_backward") # [YC] debug
         
         # return color, radii # [YC] note: main return
         return color, radii, final_opacity # [YC] note: main return
@@ -217,7 +217,7 @@ class GaussianRasterizer(nn.Module):
                 resolutions, # [YC] add
                 shs = None, colors_precomp = None, scales = None, rotations = None, cov3D_precomp = None):
         
-        print("GaussianRasterizer --> forward")
+        # print("GaussianRasterizer --> forward")
         
         raster_settings = self.raster_settings
 
